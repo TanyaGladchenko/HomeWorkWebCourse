2cf09918512d0cf87058d42d87a19fd649908745
TanyaGladchenko <t.i.gladchenko@gmail.com>
2019-04-15 16:30:47 +0300

    hw 7 task 2

---
+++ HW_7/Task_2/7-10/webapp/controller/App.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
+	"use strict";
+	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        onShowHello : function () {
+            var oBundle = this.getView().getModel("i18n").getResourceBoundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            MessageToast.show(sMsg);
+		}
+	});
+
+});
